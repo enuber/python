9475efa3a673f6efa88294ee692b121e7acaeaad
Erik Nuber <erik.nuber@yahoo.com>
2024-04-04 13:08:46 -0700

    getting started

---
+++ functions_variables/hello.py
@@ -0,0 +1,21 @@
+# code hello.py in terminal will create the file and/or open it
+# python plus file name will run the program
+# move file from folder to parent use mv filename ..
+# to run from some windows you may have to python hello.py which will pass the file to the interperter from top to bottom, left to right
+
+#function is an action or verb that lets you do some things. There are some functions that come already built in, in this case print() is a function that will print what you want it to. An arguement is what is passed into the function. A side effect is the result of running that function. in print() the side effect is to print what's placed in it.
+
+#input() lets you get input from a user. you can then store that into a variable and use it later.
+
+#variables - let you store data in them to be called later, should be called something that makes sense to what it is storing
+
+# comments for multi-lines can be done with triple quotes or just using the # on each line
+"""
+this is a comment
+"""
+
+
+name = input("What's your name? ")
+
+print("hello " + name)
+
