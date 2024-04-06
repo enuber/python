7e379964dcf03fd910eaa98ae986cb98da285b2a
Erik Nuber <erik.nuber@yahoo.com>
2024-04-06 15:49:29 -0700

    finished section on using functions from other files

--- libraries/say.py
+++ libraries/say.py
@@ -18,3 +18,25 @@ from sayings import hello
 if len(sys.argv) == 2:
   hello(sys.argv[1])
 
+
+# style pep8 standard styling
+# peps.python.org/pep-0008/
+
+# important -
+# indentation - 4 spaces
+# max line length - 
+# blank lines - makes code nicer to read
+# imports - where they go
+  
+# pylint - analyizes code for style
+# can install with pip3 install pylint
+
+# pycodestyle is better
+# pip install pycodestyle
+# pycodestyle.pycqa.org
+
+# black - even more popular/gaining popularity
+# pip3 install black
+# to run it then just use below and it will auto format properly
+# black students.py 
+  
